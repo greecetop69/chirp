ae1cd537337e41223f62bc7ebd3ed6dc8e07ee97
Andrei <bragari.andrei@carasent.com>
2024-06-13 00:24:45 +0300

    feat: Added auth via github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
-import { GeistSans } from "geist/font/sans";
-import { type AppType } from "next/app";
+import { ClerkProvider } from '@clerk/nextjs';
+import type { AppProps } from 'next/app';
 
-import { api } from "~/utils/api";
-
-import "~/styles/globals.css";
-
-const MyApp: AppType = ({ Component, pageProps }) => {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={GeistSans.className}>
+    <ClerkProvider>
       <Component {...pageProps} />
-    </main>
+    </ClerkProvider>
   );
-};
+}
 
-export default api.withTRPC(MyApp);
+export default MyApp;
